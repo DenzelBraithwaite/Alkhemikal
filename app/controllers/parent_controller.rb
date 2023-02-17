21a8e0f0838056aa504623e8085c5c0984e0d87f
Denzel Braithwaite <braithwaite.de@gmail.com>
2023-02-17 10:52:39 -0500

    added a bit to the ending

--- app/controllers/parent_controller.rb
+++ app/controllers/parent_controller.rb
@@ -249,6 +249,7 @@ end
     ending_slow_dialogue("\n#{"Glinda> ".cyan}My dear witch, my poor pathetic foolish witch. Your arrogance and greed have been judged by Satan himself.", 0.035)
     ending_slow_dialogue("\n For lack of obedience and submission, despite my countless warnings, you still ventured down the path of temptation, the path of darkness...", 0.035)
     ending_slow_dialogue("\n The path of no return. Since it's come to this, I will share with you the tale of Kazpian, the first to discover the curse of Amortentia.", 0.035)
+    sleep(3)
 
     ending_slow_dialogue("\n\n#{"Glinda> ".cyan}Kazpian was sold into slavery as a child. He was raised in the royal harem and spent his days weaving and blindly obeying his master.", 0.035)
     ending_slow_dialogue("\n He was given a glimpse of a future he would never have as he watched kids of his age study to become witches and warlocks.", 0.035)
@@ -257,6 +258,7 @@ end
     ending_slow_dialogue("\n More than anything he wanted to be free of his shackles that have long since been embedded in his heart and soul; but more than that, he wanted revenge.", 0.035)
     ending_slow_dialogue("\n Kazpian took Radea's hand as she lifted him off his knees for the last time. 'Never again will you bow down to another.", 0.035)
     ending_slow_dialogue("\n We are descendants from the great Esther herself, and we will be the past, the present and the future of this world' she said.", 0.035)
+    sleep(3)
 
     ending_slow_dialogue("\n\n#{"Glinda> ".cyan}Over the years Kazpian learnt and adapted quickly to his newfound knowledge and surroundings.", 0.035)
     ending_slow_dialogue("\n One day he was out searching for ingredients to make potions and noticed a peculiar aroma he'd never smelt before.", 0.035)
@@ -265,11 +267,13 @@ end
     ending_slow_dialogue("\n Radea couldn't bear to see her precious student lose himself in his research; before long, she reached out to me and begged for my assistance.", 0.035)
     ending_slow_dialogue("\n I had always been the more powerful, beautiful, intelligent, accomplished, beloved and renown witch between the two of us, still I reluctantly accepted.", 0.035)
     ending_slow_dialogue("\n I could see only one thing in his eyes, hunger. An insatiable thirst for something...", 0.035)
+    sleep(3)
 
     ending_slow_dialogue("\n\n#{"Glinda> ".cyan}Every day he created potions and documented the results in his journal. As time went on, I could tell he was nearing completion of his goal.", 0.035)
     ending_slow_dialogue("\n One day he returned home in a frenzy! He claimed that he had finally figured it out, but before taking action he wrote everything down in his journal.", 0.035)
     ending_slow_dialogue("\n When he was finished, he immediately began mixing potions together and brewing a potion of the likes I had never seen nor smelt before.", 0.035)
     ending_slow_dialogue("\n Something felt wrong, so I peeked in his journal and saw this page open, glaring back at me. It read:", 0.035)
+    sleep(3)
 
     ending_slow_dialogue("\n\n#{"Kaz's journal:".black.on_light_red}\n #{"Year: 1743".black.on_light_red}\n #{"Moon: ".black.on_light_red}#{"Garnet".red.on_light_red}\n\n #{"Kazpian> ".light_red}I've almost figured it out... the vial of Amortentia. I've tried and failed so many times, but this moon will be different.", 0.035)
     ending_slow_dialogue("\n I've discovered that the key ingredient isn't an ingredient at all, it's a potion!! In fact, all the ingredients are potions.", 0.035)
@@ -281,12 +285,14 @@ end
     ending_slow_dialogue("\n\n #{"Year: 1743".black.on_light_red}\n #{"Moon: ".black.on_light_red}#{"Scarlet".red.on_light_red}\n\n #{"Kazpian> ".light_red}I've almost figured it out... the vial of Amortentia. I've tried and failed so many times, but this moon will be different.", 0.035)
     ending_slow_dialogue("\n Today I discovered what my potions have been lacking... It's the #{"blood".red} of a witch.", 0.035)
     ending_slow_dialogue("\n Most likely this is what caused their disappearance, as these grounds are known to be witching territory. I must discover for myself if this witchcraf#{"-".light_red}", 0.035)
+    sleep(3)
 
     ending_slow_dialogue("\n\n#{"Glinda> ".light_red} I averted my gaze. I couldn't believe the horrors I had read; this boy was willing to sacrifice us for the possibility of creating a potion...", 0.035)
     ending_slow_dialogue("\n I grabbed his journal and shattered a vial of Netherite in my OWN home! It exploded and caught everything on fire.", 0.035)
     ending_slow_dialogue("\n I didn't look back I just kept running into the forest, destroying the book as I ran deeper and deeper...", 0.035)
     ending_slow_dialogue("\n Until I reached a dead end, when I turned back, there was nobody there. No trace I had ever been followed.", 0.035)
     ending_slow_dialogue("\n By the time I returned home, the boy was gone, the fires extinguished. I never saw him again after that, he'd stolen all my potions and ingredients.", 0.035)
+    sleep(3)
 
     ending_slow_dialogue("\n\n#{"Glinda> ".light_red} Shortly afterwards, the council came for me... Accusing me of reproducing and teaching forbidden witchcraft.", 0.035)
     ending_slow_dialogue("\n They sanctioned my studies and demanded I be burnt at the stake if I were to ever practice potion making again.", 0.035)
@@ -296,8 +302,13 @@ end
     ending_slow_dialogue("\n I changed my name to Gruntilda and severed all ties with Radea. But still I wonder... what became of Kazpian?", 0.035)
     ending_slow_dialogue("\n Did he ever get what he wanted in the end? I wonder if he'd ever come back, to finish what he started #{". . .".red}\n", 0.035, true)
     system('clear')
-    puts "You are now tinky for life.".red
-    puts "Credits: me, I did everything. :D !".blink
+    sleep(1)
+    ending_slow_dialogue("\n You are now tinky for life#{". . .".red}\n", 0.05)
+    sleep(1.5)
+    ending_slow_dialogue("\n Even after completing this a year later at 27, your tinkiness is just as potent #{"!".red}\n", 0.35)
+    sleep(1)
+    ending_slow_dialogue("\n Credits: me, I did everything. :D ! Also thanks to all my testers <3 and thanks to Elisa for finally beating the game #{"!".light_cyan}\n", 0.35)
+    sleep(2)
   end
 end
 
